db2d6af3411f1faccbd3047679fe975ef6487fd8
jankapunkt <jkuester@uni-bremen.de>
2018-05-30 16:29:08 +0200

    0.0.1

--- autoform-embed.js
+++ autoform-embed.js
@@ -1,5 +1,66 @@
-// Write your package code here!
+import { Tracker } from 'meteor/tracker';
+import { Template } from 'meteor/templating';
+import { Random } from 'meteor/random';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { $ } from 'meteor/jquery';
 
-// Variables exported by this module can be imported by other packages and
-// applications. See autoform-embed-tests.js for an example of importing.
-export const name = 'autoform-embed';
+import SimpleSchema from 'simpl-schema';
+
+import './autoform-embed.html';
+import './autoform-embed.css';
+
+// extend autoform with bpmn modeler
+AutoForm.addInputType('embed', {
+  template: 'afEmbedCode',
+  valueOut() {
+    return this.val();
+  },
+  valueIn(initialValue) {
+    return initialValue;
+  },
+});
+
+let extSchema;
+
+
+Template.afEmbedCode.onCreated(function () {
+  const instance = this;
+  instance.state = new ReactiveDict();
+  instance.state.set('initialValue', '');
+  instance.autorun(function () {
+    const { data } = instance;
+    const { atts } = data;
+    const { value } = data;
+
+    instance.state.set('dataSchemaKey', atts['data-schema-key']);
+    if (value) {
+      console.log(value);
+      $('#afEmbedCodeHiddenInput').val(value);
+      instance.state.set('initialValue', value);
+      instance.state.set('value', value);
+    }
+  });
+});
+
+Template.afEmbedCode.helpers({
+  value() {
+    return Template.instance().state.get('value');
+  },
+  dataSchemaKey() {
+    return Template.instance().state.get('dataSchemaKey')
+  },
+  initialValue() {
+    return Template.instance().state.get('initialValue');
+  }
+});
+
+Template.afEmbedCode.events({
+  'input #afEmbedCode-valueinput'(event, templateInstance) {
+    event.preventDefault();
+
+    const code = $(event.currentTarget).text();
+    templateInstance.state.set('value', code);
+    templateInstance.state.set('initialValue', '');
+    $('#afEmbedCodeHiddenInput').val(code);
+  }
+});
--- package.js
+++ package.js
@@ -2,18 +2,25 @@ Package.describe({
   name: 'jkuester:autoform-embed',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Simple AutoForm addon to enter and preview embed codes.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jankapunkt/meteor-autoform-embedcode.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.6.1.1');
+  api.versionsFrom('1.6');
   api.use('ecmascript');
-  api.mainModule('autoform-embed.js');
+  api.use('templating');
+  api.use('reactive-dict');
+  api.use('aldeed:autoform@6.0.0');
+  api.addFiles([
+    'autoform-embed.css',
+    'autoform-embed.html',
+    'autoform-embed.js',
+  ], 'client');
 });
 
 Package.onTest(function(api) {
